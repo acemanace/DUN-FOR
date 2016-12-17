b022df32280730df671848827b5ae0d4e7b14903
Don Cruver <donald.cruver@connexta.com>
2016-12-16 19:36:59 -0700

    Added tabs for TREs to both the image and file properties scroll panes.

--- codice-nitf-viewer/src/main/java/org/codice/imaging/nitf/viewer/PropertiesImageTab.java
+++ codice-nitf-viewer/src/main/java/org/codice/imaging/nitf/viewer/PropertiesImageTab.java
@@ -1,13 +1,17 @@
 package org.codice.imaging.nitf.viewer;
 
+import org.codice.imaging.nitf.core.common.TaggedRecordExtensionHandler;
 import org.codice.imaging.nitf.core.header.NitfHeader;
 import org.codice.imaging.nitf.core.image.ImageCoordinatePair;
 import org.codice.imaging.nitf.core.image.ImageCoordinates;
 import org.codice.imaging.nitf.core.image.ImageSegment;
 import org.codice.imaging.nitf.core.security.FileSecurityMetadata;
+import org.codice.imaging.nitf.core.tre.Tre;
+import org.codice.imaging.nitf.core.tre.TreEntry;
 
 import javax.swing.*;
 import java.awt.image.BufferedImage;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class PropertiesImageTab extends JSplitPane {
@@ -15,6 +19,8 @@ public class PropertiesImageTab extends JSplitPane {
     private ImagePanel imagePanel;
 
     private ImageSegment imageSegment;
+
+    private static final String[] HEADERS = {"Property", "Value"};
 
     public PropertiesImageTab(final BufferedImage bufferedImage, final NitfHeader fileHeader,
             final ImageSegment imageSegment) {
@@ -26,13 +32,15 @@ public class PropertiesImageTab extends JSplitPane {
         JScrollPane imagePropertiesScrollPane = new JScrollPane(imageProperties);
         JTabbedPane imagePropertiesTab = new JTabbedPane();
         imagePropertiesTab.addTab("Image Properties", imagePropertiesScrollPane);
+        addTreTabs(imageSegment, imagePropertiesTab);
 
         JTable fileProperties = getFilePropertyTable(fileHeader);
 
         JScrollPane filePropertiesScrollPane = new JScrollPane(fileProperties);
         JTabbedPane filePropertiesTab = new JTabbedPane();
         filePropertiesTab.addTab("File Properties", filePropertiesScrollPane);
-
+        addTreTabs(fileHeader, filePropertiesTab);
+        
         JSplitPane propertiesPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         propertiesPane.setDividerLocation(260);
         propertiesPane.setTopComponent(imagePropertiesTab);
@@ -42,6 +50,29 @@ public class PropertiesImageTab extends JSplitPane {
         this.setRightComponent(propertiesPane);
 
         SwingUtilities.invokeLater(() -> propertiesPane.setDividerLocation(0.7d));
+    }
+
+    private void addTreTabs(TaggedRecordExtensionHandler taggedRecordExtensionHandler, JTabbedPane imagePropertiesTab) {
+        taggedRecordExtensionHandler.getTREsRawStructure().getTREs().stream()
+                .forEach(tre -> {
+                    String[][] treData = convertTreToStringArray2D(tre);
+                    JTable tabProperties = new JTable(treData, HEADERS);
+                    JScrollPane treScrollPane = new JScrollPane(tabProperties);
+                    imagePropertiesTab.addTab(tre.getName(), treScrollPane);
+                });
+    }
+
+    private static String[][] convertTreToStringArray2D(Tre tre) {
+        List<TreEntry> treEntryList = tre.getEntries();
+        String[][] treData = new String[treEntryList.size()][2];
+
+        for (int i = 0; i < treEntryList.size(); i++) {
+            TreEntry treEntry = treEntryList.get(i);
+            treData[i][0] = treEntry.getName();
+            treData[i][1] = treEntry.getFieldValue();
+        }
+
+        return treData;
     }
 
     private static JTable getFilePropertyTable(NitfHeader header) {
@@ -59,8 +90,7 @@ public class PropertiesImageTab extends JSplitPane {
                 {"Class Text:", securityMetadata.getClassificationText()},
                 {"Class Reason:", securityMetadata.getClassificationReason()}};
 
-        String[] headers = {"Property", "Value"};
-        JTable fileProperties = new JTable(data, headers);
+        JTable fileProperties = new JTable(data, HEADERS);
         fileProperties.setShowGrid(false);
         return fileProperties;
     }
